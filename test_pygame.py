d8ee1a2c061239052ff3e9a3fe64397044f8bb5b
Roman Shmelev <shmelyov.roma@inbox.ru>
2021-01-06 20:08:10 +0300

    Первый коммит

--- test_pygame.py
+++ test_pygame.py
@@ -24,7 +24,7 @@ class Button():
             if event.button == 1: # is left button clicked
                 if self._rect.collidepoint(event.pos): # is mouse over button
                     self.btn_event = 0
-                    if self.pressed == 0:#здесь добавлять события к кнопкам
+                    if self.pressed == 1:#здесь добавлять события к кнопкам
                         pass
         elif (event.type == pygame.MOUSEBUTTONUP and self._rect.collidepoint(event.pos)) or \
                 (event.type == pygame.MOUSEMOTION and self._rect.collidepoint(event.pos)) and self.flag == 0:
@@ -35,45 +35,22 @@ class Button():
 
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HIGHT))
-screen.blit(SCREEN_BACKGROUND_IMAGE, (800, 450))
-manager = pygame_gui.UIManager((800, 450))
+screen = pygame.display.set_mode((800, 450))
 
 START_BUTTON = Button(START_BUTTON_IMAGE_DEFAULT, START_BUTTON_IMAGE_ACTIVE, (10, 10), (200, 100), START_BUTTON_PRESSED)
-LEVEL_ONE_BUTTON = Button(LEVEL1_BUTTON_IMAGE_DEFAULT, LEVEL1_BUTTON_IMAGE_ACTIVE,
-                          (50, SCREEN_HIGHT - 80), (190, 60), START_BUTTON_PRESSED)
-LEVEL_TWO_BUTTON = Button(LEVEL2_BUTTON_IMAGE_DEFAULT, LEVEL2_BUTTON_IMAGE_ACTIVE,
-                          (305, SCREEN_HIGHT - 80), (190, 60), START_BUTTON_PRESSED)
-LEVEL_THREE_BUTTON = Button(LEVEL3_BUTTON_IMAGE_DEFAULT, LEVEL3_BUTTON_IMAGE_ACTIVE,
-                            (SCREEN_WIDHT - 245, SCREEN_HIGHT - 80), (190, 60), START_BUTTON_PRESSED)
+
+
 running = True
 
 while running:
     screen.blit(SCREEN_BACKGROUND_IMAGE, (0, 0))
-    screen.blit(GAME_NAME, pygame.Rect((SCREEN_WIDHT - 205, 5), (200, 150)))
     #цикл обработки событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            confrimation_dialog = pygame_gui.windows.UIConfirmationDialog(
-                rect=pygame.Rect((50, 50), (300, 200)),
-                manager=manager,
-                window_title='ddd',
-                action_long_desc='ss',
-                action_short_name='ok',
-                blocking=True
-            )
-        if event.type == pygame.USEREVENT:
-            if event.type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
-                running = False
+
         START_BUTTON.pressed_event(event)
-        LEVEL_ONE_BUTTON.pressed_event(event)
-        LEVEL_TWO_BUTTON.pressed_event(event)
-        LEVEL_THREE_BUTTON.pressed_event(event)
     #отрисовка изменений
     START_BUTTON.draw(screen)
-    LEVEL_ONE_BUTTON.draw(screen)
-    LEVEL_TWO_BUTTON.draw(screen)
-    LEVEL_THREE_BUTTON.draw(screen)
     pygame.display.update()
 
 
